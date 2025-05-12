58d9c03fb98645e61c0e9acbf1e6e0028fe009d4
JakePiddington <jakepiddington@protonmail.com>
2025-05-12 21:51:00 +1000

    Add missing modules

---
+++ search/hull_helpers.py
@@ -0,0 +1,35 @@
+import numpy as np
+from numba import njit, jit, prange
+
+
+from search.hull_sutherland_hodgman import PolygonClipper
+from scipy.spatial import ConvexHull
+
+# TODO: JIT THESE
+def expand_convex_hull(hull_points, expansion_factor=0.05):
+    """Expand the convex hull by a small factor to make it slightly larger."""
+    hull_center = np.mean(hull_points, axis=0)
+    expanded_points = []
+    
+    for point in hull_points:
+        direction = point - hull_center
+        expanded_point = point + direction * expansion_factor
+        expanded_points.append(expanded_point)
+    
+    return np.array(expanded_points)
+
+def compare_hulls(hull1, hull2):
+    clipper = PolygonClipper(warn_if_empty=True)
+    clipped_polygon = clipper(hull1, hull2)
+    return clipped_polygon
+
+
+# https://stackoverflow.com/questions/57500001/numba-failure-with-np-mean
+@jit(parallel=True)
+def mean_numba(a):
+
+    res = []
+    for i in prange(a.shape[0]):
+        res.append(a[i, :].mean())
+
+    return np.array(res)
